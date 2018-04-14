51440966a41be19a9b8e9f9ea9c91078780691bf
Wong Jia Hau <hou32hou@gmail.com>
2018-04-14 23:28:59 +0800

    Added validation for non-face photo

--- examples/faceRecognition1.js
+++ examples/faceRecognition1.js
@@ -98,20 +98,28 @@ function checkIfUserIsAuthorized() {
   const detector = fr.FaceDetector()
   console.log('detecting faces')
   const faceSize = 150
+  console.log(imagePath);
   const faces = detector.detectFaces(fr.loadImage(imagePath), faceSize);
+  if(faces.length < 1) {
+    console.log("No faces detected");
+    return null;
+  }
   fr.saveImage(imagePath, faces[0]);
   recognizer.load(require(trainedModelFilePath));
   const prediction = recognizer.predictBest(faces[0]);
   console.log(prediction);
   const CUT_POINT = 0.45; // The CUT_POINT is defined based on heuristic
   if(prediction.distance < CUT_POINT) {
+    console.log("Face recognized.");
     return prediction.className;
   }
+  console.log("Unrecognized faced");
   return null;
 }
 
 // trainData();
 // predictDataDemo();
+// checkIfUserIsAuthorized();
 
 
 module.exports = {trainData, checkIfUserIsAuthorized};
