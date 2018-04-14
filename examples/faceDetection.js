d7774cf3923d9dfdcc70a17a8b14f80d09dc8177
Wong Jia Hau <hou32hou@gmail.com>
2018-04-15 05:23:21 +0800

    Fix some bug in cropFaceOut

--- examples/faceDetection.js
+++ examples/faceDetection.js
@@ -19,14 +19,12 @@ function cropFaceOut(directory) {
         const img = fr.loadImage(directory + path);
         const faceRectangles = frontalDetector.detect(img);
         const biggestRectangle = faceRectangles.sort(x => x.area).reverse()[0];
-
-
+        if(!biggestRectangle) {
+            return false;
+        }
         console.log('detecting faces for ' + path)
         const faceSize = 150
         const faces = detector.getFacesFromLocations(img, [biggestRectangle], faceSize);
-        if(faces.length === 0) {
-            return false;
-        }
         fr.saveImage(directory + path, faces[0]);
     });
     return true;
