736b230af671eb4434bd5ee84373349eeef7b54a
Wong Jia Hau <hou32hou@gmail.com>
2018-04-14 18:29:40 +0800

    Settle api for checkout also

--- examples/server.js
+++ examples/server.js
@@ -2,6 +2,7 @@
 
 // BASE SETUP
 // =============================================================================
+const DEST_IP = '192.168.20.107';
 
 // call the packages we need
 const express    = require('express');        // call express
@@ -9,6 +10,8 @@ const app        = express();                 // define our app using express
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const checkIfUserIsAuthorized = require("./faceRecognition1").checkIfUserIsAuthorized;
+const request = require('request');
+
  
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -27,45 +30,59 @@ router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
-router.post('/checkIn', function(req, res) {
+router.post('/checkin', function(req, res) {
     if (!req.files) {
         return res.status(400).send('No files were uploaded.');
     }
-    
-    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-    console.log(req.files);
+    checkUser(req.files, (response) => {
+        res.send(response);
+        request.post(
+            `http://${DEST_IP}:8080/api/checkin`, 
+            {json: response},
+            function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body)
+                }
+            }
+        );
+    })
+});
 
-    for(var filename in req.files) {
-        const file = req.files[filename];
-        console.log(file);
+router.post('/checkout', function(req, res) {
+    if (!req.files) {
+        return res.status(400).send('No files were uploaded.');
+    }
+    checkUser(req.files, (response) => {
+        res.send(response);
+        request.post(
+            `http://${DEST_IP}:8080/api/checkout`, 
+            {json: response},
+            function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    console.log(body)
+                }
+            }
+        );
+    })
+});
+
+function checkUser(files, callback) {
+    for(var filename in files) {
+        const file = files[filename];
         file.mv('./toBeClassified/temp.png', (err) => {
             if(err) {
-                return res.status(500).send(err);
+                console.log(err);
+                return;
             }
             var username = checkIfUserIsAuthorized()
-            if(username) {
-                res.send({status : "SUCCESS", data: {username: username}});
-            } else {
-                res.send({status : "FAIL"});
-            }
+            const response = username ? 
+                {status : "SUCCESS", data: {username: username}, timestamp: new Date().getTime()} :
+                {status : "FAIL", timestamp: new Date().getTime()};
+            callback(response);
         });
     }
 
-    // let sampleFile = req.files.sampleFile;
-    
-    // // Use the mv() method to place the file somewhere on your server
-    // sampleFile.mv('./', function(err) {
-    //     if (err)
-    //     return res.status(500).send(err);
-    
-    //     res.send('File uploaded!');
-    // });
-    // res.json({ message: 'hooray! welcome to our api!' });   
-});
-
-router.get('/checkout', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
+}
 
 router.get('/register', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
